d0e2db1c488278a6625463777ad32f34291f156e
juliomoreira666 <julio@codeby.com.br>
2018-03-07 01:47:26 -0300

    Initial Commit

---
+++ src/app/giphy/giphy-search-manual/giphy-search-manual.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'giphy-search-manual',
+    templateUrl: 'giphy-search-manual.component.html'
+})
+
+export class GiphySearchManualComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
+
---
+++ src/app/giphy/giphy-search-manual/giphy-search-manual.module.ts
@@ -0,0 +1,12 @@
+import { GiphySearchManualComponent } from './giphy-search-manual.component';
+import { NgModule } from '@angular/core';
+
+
+
+@NgModule({
+    imports: [],
+    exports: [GiphySearchManualComponent],
+    declarations: [GiphySearchManualComponent],
+    providers: [],
+})
+export class GiphySeacrhManualComponentModule { }
